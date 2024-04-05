ef3a27f9f00297734babdb21f550ec71cb2049f1
JuubiNaNgai <antoinelb60170@gmail.com>
2024-04-05 17:38:27 +0200

    debut of test

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aleite-b <aleite-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 17:38:19 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/04/05 14:55:58 by aleite-b         ###   ########.fr       */
+/*   Updated: 2024/04/05 17:33:13 by aleite-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,19 +120,135 @@ int	main(int argc, char **argv)
             }
             if (it == servers.end())
                 client_fd = events[i].data.fd;
-
-            if ((client_fds[client_fd].getBuffer().find("\r\n\r\n") == std::string::npos) && (events[i].events & EPOLLIN))
+            
+            size_t pos = client_fds[client_fd].getBuffer().find("\r\n\r\n");
+            if ((pos == std::string::npos) && (events[i].events & EPOLLIN))
             {
-                char buffer[1024] = {0};
-                valread = recv(client_fd, buffer, 1024, MSG_DONTWAIT);
-                if (valread < 1)
-                {
-                    if (errno == EAGAIN || errno == EWOULDBLOCK)
+                // std::string check_to_read;
+                // if (client_fds[client_fd].getBuffer().empty())
+                //     check_to_read = "";
+                // else
+                //     check_to_read = client_fds[client_fd].getBuffer().substr(pos + 4, client_fds[client_fd].getBuffer().size() + 4);
+                // std::cout << "====== " << check_to_read << std::endl;
+                // if (check_to_read.find("\r\n\r\n") != std::string::npos)
+                // {
+                    char buffer[1024] = {0};
+                    valread = recv(client_fd, buffer, 1024, MSG_DONTWAIT);
+                    if (valread < 1)
                     {
-                        std::cerr << "Socket Unblocked : " << std::endl;
+                        if (errno == EAGAIN || errno == EWOULDBLOCK)
+                        {
+                            std::cerr << "Socket Unblocked : " << std::endl;
+                            continue;
+                        }
+                        std::cerr << "Error Reading : " << std::endl;
+                        client_fds.erase(client_fd);
+                        if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL) < 0)
+                        {
+                            std::cerr << "Error Deleting EPOLL : " << std::strerror(errno) << std::endl;
+                            exit(EXIT_FAILURE);
+                        }
+                        close(client_fd);
                         continue;
                     }
-                    std::cerr << "Error Reading : " << std::endl;
+                    client_fds[client_fd].addToBuffer(buffer);
+
+                    // std::cout << "=========================================================" << std::endl;
+                    // std::cout << buffer << std::endl;
+                    // std::cout << "=========================================================" << std::endl;
+                    
+                    // for (it = servers.begin(); it != servers.end(); it++)
+                    // {
+                    //     WebServ* server = *it;
+                    //     if (client_fds[client_fd].getServerFd() == server->getServerFd()) {
+                    //         if ((long)client_fds[client_fd].getBufferSize() > server->getMaxBodySize())
+                    //         {
+                    //             //Send err : "Payload Too Large" (code d'état HTTP 413).
+                    //             client_fds.erase(client_fd);
+                    //             if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL) < 0)
+                    //             {
+                    //                 std::cerr << "Error Deleting EPOLL : " << std::strerror(errno) << std::endl;
+                    //                 exit(EXIT_FAILURE);
+                    //             }
+                    //             close(client_fd);
+                    //             std::cout << "_____________________________________________________________________________________________________________________________" << std::endl;
+                    //             std::cout << "Max body reached" << std::endl;
+                    //             continue;
+                    //         }
+                    //         break;
+                    //     }
+                    // }
+                    // std::cout << buffer << " - " << valread << std::endl;
+                    std::cout << "=============================================" << std::endl;
+                    std::cout << client_fds[client_fd].getBuffer() << std::endl;
+                    std::cout << "=============================================" << std::endl;
+                // }
+            }
+            
+            pos = client_fds[client_fd].getBuffer().find("\r\n\r\n");
+            if (pos != std::string::npos)
+            {
+                // std::string check_to_send = client_fds[client_fd].getBuffer().substr(pos + 4, client_fds[client_fd].getBuffer().size() + 4);
+                // if (check_to_send.find("\r\n\r\n") != std::string::npos)
+                // {
+                    Request req(client_fds[client_fd].getBuffer());
+                    std::string response;
+                    
+                    for (it = servers.begin(); it != servers.end(); it++)
+                    {
+                        WebServ* server = *it;
+                        if (client_fds[client_fd].getServerFd() == server->getServerFd()) {
+                            WebConfig config = server->getConfig();
+                            struct RouteConfig route;
+                            
+                            if (config.routes.find(req.getPath()) != config.routes.end())
+                            {
+                                route = config.routes.find(req.getPath())->second;
+                            }
+                            else
+                            {
+                                route = config.routes.find("/")->second;
+                            }
+                            std::cout << req.getMethod() << std::endl;
+                            
+                            if (req.getMethod() == "GET" && (std::find(route.limit_except_accepted.begin(), route.limit_except_accepted.end(), req.getMethod()) != route.limit_except_accepted.end()))
+                            {
+                                response = handleGET(req, route);
+                            }
+                            else if (req.getMethod() == "POST" && (std::find(route.limit_except_accepted.begin(), route.limit_except_accepted.end(), req.getMethod()) != route.limit_except_accepted.end()))
+                            {
+                                response = handlePOST(req, route);
+                            }
+                            else if (req.getMethod() == "DELETE" && (std::find(route.limit_except_accepted.begin(), route.limit_except_accepted.end(), req.getMethod()) != route.limit_except_accepted.end()))
+                                response = handleDELETE(req, route);
+                            else
+                            {
+                                response = getErrorsPages("501");
+                            }
+                            break;
+                        }
+                    }
+                    // if (req.getMethod() == "GET")
+                    // {
+                    //     response = handleGET(req);
+                    // }
+                    // else if (req.getMethod() == "POST")
+                    // {
+                    //     response = handlePOST(req);
+                    // }
+                    // else if (req.getMethod() == "DELETE")
+                    //     response = handleDELETE(req);
+                    // else
+                    // {
+                    //     response = getErrorsPages("501");
+                    // }
+                    req.printHeaders();
+                    std::cout << std::endl << std::endl << "Response: \n"<< response << std::endl;
+                    std::string hello = response;
+                    if (send(client_fd, hello.c_str(), hello.length(), 0) != static_cast<long int>(hello.length()))
+                    {
+                        std::cerr << "Error Sending : " << std::endl;
+                    }
                     client_fds.erase(client_fd);
                     if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL) < 0)
                     {
@@ -140,107 +256,7 @@ int	main(int argc, char **argv)
                         exit(EXIT_FAILURE);
                     }
                     close(client_fd);
-                    continue;
-                }
-                client_fds[client_fd].addToBuffer(buffer);
-
-                // std::cout << "=========================================================" << std::endl;
-                // std::cout << buffer << std::endl;
-                // std::cout << "=========================================================" << std::endl;
-                
-                // for (it = servers.begin(); it != servers.end(); it++)
-                // {
-                //     WebServ* server = *it;
-                //     if (client_fds[client_fd].getServerFd() == server->getServerFd()) {
-                //         if ((long)client_fds[client_fd].getBufferSize() > server->getMaxBodySize())
-                //         {
-                //             //Send err : "Payload Too Large" (code d'état HTTP 413).
-                //             client_fds.erase(client_fd);
-                //             if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL) < 0)
-                //             {
-                //                 std::cerr << "Error Deleting EPOLL : " << std::strerror(errno) << std::endl;
-                //                 exit(EXIT_FAILURE);
-                //             }
-                //             close(client_fd);
-                //             std::cout << "_____________________________________________________________________________________________________________________________" << std::endl;
-                //             std::cout << "Max body reached" << std::endl;
-                //             continue;
-                //         }
-                //         break;
-                //     }
                 // }
-                // std::cout << buffer << " - " << valread << std::endl;
-                std::cout << client_fds[client_fd].getBuffer() << std::endl;
-                
-            }
-            
-            if (client_fds[client_fd].getBuffer().find("\r\n\r\n") != std::string::npos || client_fds[client_fd].getBuffer().find("\r\r\n") != std::string::npos)
-            {
-                Request req(client_fds[client_fd].getBuffer());
-                std::string response;
-                
-                for (it = servers.begin(); it != servers.end(); it++)
-                {
-                    WebServ* server = *it;
-                    if (client_fds[client_fd].getServerFd() == server->getServerFd()) {
-                        WebConfig config = server->getConfig();
-                        struct RouteConfig route;
-                        
-                        if (config.routes.find(req.getPath()) != config.routes.end())
-                        {
-                            route = config.routes.find(req.getPath())->second;
-                        }
-                        else
-                        {
-                            route = config.routes.find("/")->second;
-                        }
-                        std::cout << req.getMethod() << std::endl;
-                        
-                        if (req.getMethod() == "GET" && (std::find(route.limit_except_accepted.begin(), route.limit_except_accepted.end(), req.getMethod()) != route.limit_except_accepted.end()))
-                        {
-                            response = handleGET(req, route);
-                        }
-                        else if (req.getMethod() == "POST" && (std::find(route.limit_except_accepted.begin(), route.limit_except_accepted.end(), req.getMethod()) != route.limit_except_accepted.end()))
-                        {
-                            response = handlePOST(req, route);
-                        }
-                        else if (req.getMethod() == "DELETE" && (std::find(route.limit_except_accepted.begin(), route.limit_except_accepted.end(), req.getMethod()) != route.limit_except_accepted.end()))
-                            response = handleDELETE(req, route);
-                        else
-                        {
-                            response = getErrorsPages("501");
-                        }
-                        break;
-                    }
-                }
-                // if (req.getMethod() == "GET")
-                // {
-                //     response = handleGET(req);
-                // }
-                // else if (req.getMethod() == "POST")
-                // {
-                //     response = handlePOST(req);
-                // }
-                // else if (req.getMethod() == "DELETE")
-                //     response = handleDELETE(req);
-                // else
-                // {
-                //     response = getErrorsPages("501");
-                // }
-                req.printHeaders();
-                std::cout << std::endl << std::endl << "Response: \n"<< response << std::endl;
-                std::string hello = response;
-                if (send(client_fd, hello.c_str(), hello.length(), 0) != static_cast<long int>(hello.length()))
-                {
-                    std::cerr << "Error Sending : " << std::endl;
-                }
-                client_fds.erase(client_fd);
-                if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL) < 0)
-                {
-                    std::cerr << "Error Deleting EPOLL : " << std::strerror(errno) << std::endl;
-                    exit(EXIT_FAILURE);
-                }
-                close(client_fd);
             }
         }
     }
